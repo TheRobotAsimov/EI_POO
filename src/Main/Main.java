60050c2800d59ce67cc5152893b022a7e73675a6
Kevin <kevinyahirt@gmail.com>
2024-08-10 22:10:06 -0600

    Connection done

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -5,8 +5,8 @@ package Main;
 
 public class Main {
 
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws Exception {
+
     }
     
 }
